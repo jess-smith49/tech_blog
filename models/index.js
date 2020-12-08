b75a182ce0f691fca2d20c2afa3dc7599df79d84
jess-smith49 <js.jessicasmith774@gmail.com>
2020-12-07 23:08:06 -0600

    associations started

--- models/index.js
+++ models/index.js
@@ -1,3 +1,26 @@
+//getting all of the models
 const User = require('./user');
 const Blog = require('./blog');
 const Comment = require('./comment');
+
+//creating associations
+
+//user has many blog posts
+User.hasMany(Blog, {
+    foreignKey: 'user_id'
+});
+
+//blog posts have one user
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+//user can belong to many blog posts
+User.belongsToMany(Blog, {
+
+})
+
+//
+
+//exporting 
+module.exports = {User, Blog, Comment};
